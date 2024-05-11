218c8ffeff793d194fe9e1270fad64cd33f4d067
iibrahim70 <iibrahiim.dev@gmail.com>
2024-05-11 13:59:32 +0600

    refactor products  and product  details skeleton

--- src/app/(mainLayout)/[products]/[slug]/loading.tsx
+++ src/app/(mainLayout)/[products]/[slug]/loading.tsx
@@ -3,17 +3,17 @@ const ProductDetailsLoading = () => {
     <main className="min-h-[calc(100dvh-64px)] section-wrapper py-10 flex   items-center justify-center">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 w-full">
         {/* skeleton for image gallery  */}
-        <section className="rounded-md shadow-md border dark:border-[#212121] cursor-pointer">
+        <section className="rounded-md shadow-md border dark:border-jet-gray cursor-pointer">
           <div className="animate-pulse">
             {/* large images */}
-            <div className="rounded-t-md h-80 bg-[#d0d0d0] dark:bg-[#424242]" />
+            <div className="rounded-t-md h-80 bg-light-gray dark:bg-dark-gray" />
 
             {/* small images */}
             <div className="grid grid-cols-4 gap-5 p-5">
               {Array.from({ length: 4 }, (_, index) => (
                 <div
                   key={index}
-                  className="rounded-md border dark:border-[#212121] h-20 bg-[#d0d0d0] dark:bg-[#424242]"
+                  className="rounded-md border dark:border-jet-gray h-20 bg-light-gray dark:bg-dark-gray"
                 />
               ))}
             </div>
@@ -23,28 +23,28 @@ const ProductDetailsLoading = () => {
         {/* skeleton for product content */}
         <section>
           <div className="animate-pulse space-y-10">
-            <div className="h-3 w-1/2 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+            <div className="h-3 w-1/2 rounded-full bg-light-gray dark:bg-dark-gray" />
 
             <div className="space-y-1.5">
-              <div className="h-1.5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
-              <div className="h-1.5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
-              <div className="h-1.5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+              <div className="h-1.5 rounded-full bg-light-gray dark:bg-dark-gray" />
+              <div className="h-1.5 rounded-full bg-light-gray dark:bg-dark-gray" />
+              <div className="h-1.5 rounded-full bg-light-gray dark:bg-dark-gray" />
             </div>
 
             <div className="space-y-3.5">
               <div className="w-1/2 flex items-center gap-10">
-                <div className="h-3 w-1/4 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
-                <div className="h-3 w-1/4 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+                <div className="h-3 w-1/4 rounded-full bg-light-gray dark:bg-dark-gray" />
+                <div className="h-3 w-1/4 rounded-full bg-light-gray dark:bg-dark-gray" />
               </div>
 
               <div className="w-1/2 flex items-center gap-10">
-                <div className="h-3 w-2/5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
-                <div className="h-3 w-2/5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+                <div className="h-3 w-2/5 rounded-full bg-light-gray dark:bg-dark-gray" />
+                <div className="h-3 w-2/5 rounded-full bg-light-gray dark:bg-dark-gray" />
               </div>
 
               <div className="flex items-center justify-between gap-10">
-                <div className="h-6 w-2/5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
-                <div className="h-6 w-2/5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+                <div className="h-6 w-2/5 rounded-full bg-light-gray dark:bg-dark-gray" />
+                <div className="h-6 w-2/5 rounded-full bg-light-gray dark:bg-dark-gray" />
               </div>
             </div>
 
@@ -52,7 +52,7 @@ const ProductDetailsLoading = () => {
               {Array.from({ length: 15 }, (_, index) => (
                 <div
                   key={index}
-                  className="h-1.5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]"
+                  className="h-1.5 rounded-full bg-light-gray dark:bg-dark-gray"
                 />
               ))}
             </div>
--- src/app/(mainLayout)/[products]/loading.tsx
+++ src/app/(mainLayout)/[products]/loading.tsx
@@ -1,23 +1,23 @@
 const ProductsLoading = () => {
   return (
     <main className="section-wrapper py-10 grid grid-cols-1 lg:grid-cols-5 gap-10">
-      {/* filter skeleton */}
+      {/* skeleton for products filter  */}
       <section className="max-md:hidden lg:col-span-1 space-y-5">
         {Array.from({ length: 3 }, (_, index) => (
           <div
             key={index}
-            className="rounded-md shadow-md border dark:border-[#212121] p-5 cursor-pointer"
+            className="rounded-md shadow-md border dark:border-jet-gray p-5 cursor-pointer"
           >
             <div className="animate-pulse">
-              <div className="h-4 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+              <div className="h-4 rounded-full bg-light-gray dark:bg-dark-gray" />
 
-              <div className="h-1 rounded-full my-5 bg-[#d0d0d0] dark:bg-[#424242]" />
+              <div className="h-1 rounded-full my-5 bg-light-gray dark:bg-dark-gray" />
 
               <div className="space-y-2">
                 {Array.from({ length: 5 }, (_, index) => (
                   <div
                     key={index}
-                    className="h-2.5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]"
+                    className="h-2.5 rounded-full bg-light-gray dark:bg-dark-gray"
                   />
                 ))}
               </div>
@@ -26,33 +26,33 @@ const ProductsLoading = () => {
         ))}
       </section>
 
-      {/* products skeleton */}
+      {/* skeleton for products card */}
       <section className="lg:col-span-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
         {Array.from({ length: 6 }, (_, index) => (
           <div
             key={index}
-            className="shadow-md rounded-md border dark:border-[#212121] cursor-pointer"
+            className="shadow-md rounded-md border dark:border-jet-gray cursor-pointer"
           >
             <div className="animate-pulse">
-              <div className="size-60 w-full rounded-t-md bg-[#d0d0d0] dark:bg-[#424242]" />
+              <div className="size-60 w-full rounded-t-md bg-light-gray dark:bg-dark-gray" />
 
               <div className="px-3 py-5 space-y-5">
-                <div className="h-3 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+                <div className="h-3 rounded-full bg-light-gray dark:bg-dark-gray" />
 
                 <div className="space-y-1.5">
-                  <div className="h-1.5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
-                  <div className="h-1.5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
-                  <div className="h-1.5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+                  <div className="h-1.5 rounded-full bg-light-gray dark:bg-dark-gray" />
+                  <div className="h-1.5 rounded-full bg-light-gray dark:bg-dark-gray" />
+                  <div className="h-1.5 rounded-full bg-light-gray dark:bg-dark-gray" />
                 </div>
 
                 <div className="flex items-center justify-between gap-20">
-                  <div className="h-3 w-1/4 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
-                  <div className="h-3 w-1/4 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+                  <div className="h-3 w-1/4 rounded-full bg-light-gray dark:bg-dark-gray" />
+                  <div className="h-3 w-1/4 rounded-full bg-light-gray dark:bg-dark-gray" />
                 </div>
 
                 <div className="flex items-center justify-between gap-20">
-                  <div className="h-6 w-2/5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
-                  <div className="h-6 w-2/5 rounded-full bg-[#d0d0d0] dark:bg-[#424242]" />
+                  <div className="h-6 w-2/5 rounded-full bg-light-gray dark:bg-dark-gray" />
+                  <div className="h-6 w-2/5 rounded-full bg-light-gray dark:bg-dark-gray" />
                 </div>
               </div>
             </div>
