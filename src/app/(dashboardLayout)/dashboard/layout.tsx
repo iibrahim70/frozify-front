22a071b4b53f3b7a925bd9dde7f769b6c2155bf8
iibrahim70 <iibrahiim.dev@gmail.com>
2024-05-17 15:35:57 +0600

    [WORKING] update layout making custom

--- src/app/(dashboardLayout)/dashboard/layout.tsx
+++ src/app/(dashboardLayout)/dashboard/layout.tsx
@@ -1,51 +1,48 @@
-"use client";
-
 import { ReactNode } from "react";
-import { Layout, Menu, MenuProps } from "antd";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
-const { Content, Sider } = Layout;
-const items: MenuProps["items"] = [
+const navItems = [
   {
     key: "1",
-    label: <Link href="/">Home</Link>,
+    href: "/",
+    label: "Home",
   },
   {
     key: "2",
-    label: <Link href="/dashboard/all-products">All Products</Link>,
+    href: "/dashboard/all-products",
+    label: "All Products",
   },
 ];
 
 const dashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
-      <Layout className="min-h-[calc(100dvh-64px)]">
-        <Sider breakpoint="lg" collapsedWidth="0">
-          <Link
-            href="/"
-            className="font-bold text-xl py-5 w-full inline-block text-center text-white"
-          >
-            Frozify
-          </Link>
+    <main className="min-h-dvh flex justify-between gap-10">
+      <div className="w-[15%] p-5 bg-jet-gray">
+        <Link
+          href="/"
+          className="inline-block w-full font-bold text-xl text-center text-white"
+        >
+          Frozify
+        </Link>
 
-          <hr className="w-[90%] mx-auto" />
+        <hr className="w-[90%] mx-auto my-5" />
 
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            items={items}
-            className="mt-10"
-          />
-        </Sider>
+        <div className="flex flex-col items-start w-full">
+          {navItems?.map((item) => (
+            <Link
+              key={item?.key}
+              href={item?.href}
+              className="w-full py-2 px-3 rounded-md duration-300 ease-in-out bg-transparent hover:bg-black/85"
+            >
+              {item?.label}
+            </Link>
+          ))}
+        </div>
+      </div>
 
-        <Layout>
-          {/* content part */}
-          <Content className="bg-white dark:bg-black dark:text-white p-10">
-            {children}
-          </Content>
-        </Layout>
-      </Layout>
+      <div className="w-[85%] bg-green-500">{children}</div>
     </main>
   );
 };
